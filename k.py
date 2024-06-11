32ebce4f2d277e8bcaa8e40345895741c0ea3e4f
mattwoods01 <137658418+mattwoods01@users.noreply.github.com>
2024-06-10 20:36:53 -0700

    Update k.py
    add final changes

--- k.py
+++ k.py
@@ -24,14 +24,14 @@ dt = 0.1  # Time step (s)
 wall_temp_duration = 50 # Number of Wall Temperature 'ON' time steps
 remove_wall_after = False # Remove Wall if True, disables fourth boundry condition on boundry border
 fourth_boundary_on = True
-time_steps = 1000  # Number of time steps
+time_steps = 100  # Number of time steps
 tau_q = 600  # Relaxation time due to heat flux (s)
 tau_T = 300  # Relaxation time due to temperature gradient (s)
 tau_v = 100  # Relaxation time due to thermal displacement (s)
 ambient_temp = 37 # Ambient temperature of space without initialized wall temp (°C)
 
 # Constants for the second material (assuming for the boundary condition of fourth kind)
-ku = 0.625  # Thermal conductivity of left material (W/m°C)
+ku = 0.625   # Thermal conductivity of left material (W/m°C)
 
 DOI_1 = 'https://doi.org/10.1016/j.ijthermalsci.2022.108002'
 
@@ -58,7 +58,7 @@ def convective_boundary(T_array):
 # Fourth boundary using constant temperature and heat flux for two thermal conductivity terms from secondary paper to model conduction.
 def fourth_boundary(T_array):
     if fourth_boundary_on is True:
-        T_array[-1, :] = T_array[-2, :] - (k / ku) * (T_array[-2, :] - T_array[-3, :]) # x = 0
+        T_array[-1, :] = T_array[-2, :] - (k / ku) * (T_array[-2, :] - T_array[-3, :])  # x = 0
         T_array[:, 0] = T_array[:, 1] - (ku / k) * (T_array[:, 1] - T_array[:, 2])  # y = 0
 
 # Third boundary using constant heat coefficient 
@@ -159,6 +159,10 @@ for k in k_list:
         elif t < wall_temp_duration or remove_wall_after is False:
             print("on")
             fourth_boundary(T_new)
+
+        elif t < wall_temp_duration or remove_wall_after is True:
+            print("on")
+            third_boundary(T_new)
 
         # Reapply fixed temperature boundary condition at each time step
         if t < wall_temp_duration:
